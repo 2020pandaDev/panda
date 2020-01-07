568faeb06090fb6c68ce96fd6abd29e19a9d88b3
liupei <59087972+deepin0807@users.noreply.github.com>
2020-01-07 20:31:13 +0800

    Bella 002 (#37)
    * remove rabbish
    
    * insert userinfo

--- server/IM_Server/IM_Server_one/server.cpp
+++ server/IM_Server/IM_Server_one/server.cpp
@@ -29,7 +29,7 @@ void Server::incomingConnection(qintptr socketDescriptor)
     //信号与槽函数在同一个线程，则用直连方式Qt::DirectConnection，若信号是跨线程的，则使用排队连接方式
     QObject::connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()), Qt::DirectConnection);
     connect(thread, SIGNAL(work1()), worker, SLOT(dowork()), Qt::DirectConnection);
-    connect(thread, SIGNAL(regist()), worker, SLOT(registe()), Qt::DirectConnection);
+    connect(thread, SIGNAL(regist(QStringList &)), worker, SLOT(registe(QStringList &)), Qt::DirectConnection);
     connect(thread, SIGNAL(login(QStringList &)), worker, SLOT(loginIn(QStringList &)), Qt::DirectConnection);
     connect(thread, SIGNAL(dowithCAPTCHA()), worker, SLOT(doingCAPTCHA()), Qt::DirectConnection);
 //    connect(thread, SIGNAL(work1()), worker, SLOT(dowork()), Qt::DirectConnection);
