6cde20ec2bc656d27005c7cf6b167cdcd49292d5
年向 <45691758+shanccshuirr@users.noreply.github.com>
2020-01-13 02:42:14 -0600

    增加注释 (#122)

--- server/IM_Server/IM_Server_one/server.cpp
+++ server/IM_Server/IM_Server_one/server.cpp
@@ -4,16 +4,16 @@
 Server::Server(QObject *parent) : QObject(parent)
 {
     m_ctcpsv =new CTcpSv(this);
-    m_ctcpsv->listen(QHostAddress::Any,6565);
-    connect(m_ctcpsv,&CTcpSv::sendSocket,this,&Server::incomingConnection);
+    m_ctcpsv->listen(QHostAddress::Any,6565);//监听
+    connect(m_ctcpsv,&CTcpSv::sendSocket,this,&Server::incomingConnection);//客户端连接调用
 }
 
 void Server::incomingConnection(qintptr socketDescriptor)
 {
 
     ServerThread* thread = new ServerThread(socketDescriptor);
-    Worker* worker = new Worker ();
-    worker->moveToThread(thread);
+    Worker* worker = new Worker ();//线程工作类
+    worker->moveToThread(thread);//线程工作类移动到子线程下
 
     //信号与槽函数在同一个线程，则用直连方式Qt::DirectConnection，若信号是跨线程的，则使用排队连接方式
     QObject::connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()), Qt::DirectConnection);//
