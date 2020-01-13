bf9b7c2a40549ea8f0e39f65b355acc66bb520f1
shanccshuirr <kdnxq2050@163.com>
2020-01-13 10:57:39 +0800

    内存释放

--- server/IM_Server/IM_Server_one/serverthread.cpp
+++ server/IM_Server/IM_Server_one/serverthread.cpp
@@ -10,17 +10,13 @@ ServerThread::ServerThread(qintptr socketDescriptor)
     qDebug() << "main thread:" << currentThreadId();
 }
 
-
-
-
 void ServerThread::run()
 {
 
     emit sendSocketDescriptor(m_socketDescriptor);
 //       m_tcpSocket->disconnectFromHost(); // 这个函数是异步执行的
 //       m_tcpSocket->waitForDisconnected(); // 注意到这又是个waitFor...()函数，它会阻塞当前线程直到连接断开
-       exec();
-
+    exec();
 }
 
 
