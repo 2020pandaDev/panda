988119c4e22c44eb19d845f4a24d8fc65bb61c7a
liupei <59087972+deepin0807@users.noreply.github.com>
2020-01-13 15:58:47 +0800

    add note (#119)

--- server/IM_Server/IM_Server_one/worker.h
+++ server/IM_Server/IM_Server_one/worker.h
@@ -22,16 +22,16 @@ signals:
 
 
 public slots:
-    void doWork(QByteArray& message);
-    QVariantMap registe(QStringList & registerInfo);
-    QVariantMap updateUserList();
-    QVariantMap loginIn(QStringList & userInfoList);
-    QVariantMap privateChat(QVariantMap&);
+    void doWork(QByteArray& message);//入口函数
+    QVariantMap registe(QStringList & registerInfo);//注册函数
+    QVariantMap updateUserList();//更新用户名列表函数
+    QVariantMap loginIn(QStringList & userInfoList);//登录函数
+    QVariantMap privateChat(QVariantMap&);//私聊函数
     QVariantMap doingCAPTCHA(QStringList &CAPTCHAInfo);
     QVariantMap helpingOther(QStringList &HelpingInfo);
 
-    QVariantMap Signout(QStringList &SignoutInfo);
-    QVariantMap groupChat(QVariantMap &groupChatInfo);
+    QVariantMap Signout(QStringList &SignoutInfo);//退出函数
+    QVariantMap groupChat(QVariantMap &groupChatInfo);//群聊函数
     void recSocketDescriptor(qintptr);
 
 private:
@@ -39,6 +39,7 @@ private:
     QMap<QString ,QString> userVerification;
     QTcpSocket* m_tcpSocket = nullptr;
     QByteArray m_recData;
+    QTimer *timer;
 
 
 };
